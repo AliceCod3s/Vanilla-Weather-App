6298e3c9c7965201031b550935e8102aa3165444
Alice <alicewrites@hotmail.com>
2021-01-17 00:58:08 +0200

    Added the search engine

--- src/index.js
+++ src/index.js
@@ -33,9 +33,22 @@ function displayTemperature(response){
     iconElement.setAttribute("alt", response.data.weather[0].description);
 
 }
+function search(city) {
 
 let apiKey = "7df1346f7ba4848042617fafa19ef55a";
-let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
